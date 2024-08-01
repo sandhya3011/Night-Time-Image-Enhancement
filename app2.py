c1cce09c4f029463ed55cb08cf8618e4afb1e524
sandhya3011 <biyyamsandhya12@gmail.com>
2024-08-01 16:13:49 +0530

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,336 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+import os
+
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+import random
+import numpy as np
+from glob import glob
+from PIL import Image, ImageOps
+import matplotlib.pyplot as plt
+
+import keras
+from keras import layers
+
+import tensorflow as tf
+
+from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint,Callback
+
+# Define MirNet model architecture
+
+
+# Recursive Residual Modules
+
+random.seed(10)
+
+IMAGE_SIZE = 128
+BATCH_SIZE = 2
+MAX_TRAIN_IMAGES = 300
+
+
+def read_image(image_path):
+    image = tf.io.read_file(image_path)
+    image = tf.image.decode_png(image, channels=3)
+    image.set_shape([None, None, 3])
+    image = tf.cast(image, dtype=tf.float32) / 255.0
+    return image
+
+
+def random_crop(low_image, enhanced_image):
+    low_image_shape = tf.shape(low_image)[:2]
+    low_w = tf.random.uniform(
+        shape=(), maxval=low_image_shape[1] - IMAGE_SIZE + 1, dtype=tf.int32
+    )
+    low_h = tf.random.uniform(
+        shape=(), maxval=low_image_shape[0] - IMAGE_SIZE + 1, dtype=tf.int32
+    )
+    low_image_cropped = low_image[
+        low_h : low_h + IMAGE_SIZE, low_w : low_w + IMAGE_SIZE
+    ]
+    enhanced_image_cropped = enhanced_image[
+        low_h : low_h + IMAGE_SIZE, low_w : low_w + IMAGE_SIZE
+    ]
+    # in order to avoid `NONE` during shape inference
+    low_image_cropped.set_shape([IMAGE_SIZE, IMAGE_SIZE, 3])
+    enhanced_image_cropped.set_shape([IMAGE_SIZE, IMAGE_SIZE, 3])
+    return low_image_cropped, enhanced_image_cropped
+
+
+def load_data(low_light_image_path, enhanced_image_path):
+    low_light_image = read_image(low_light_image_path)
+    enhanced_image = read_image(enhanced_image_path)
+    low_light_image, enhanced_image = random_crop(low_light_image, enhanced_image)
+    return low_light_image, enhanced_image
+
+
+def get_dataset(low_light_images, enhanced_images):
+    dataset = tf.data.Dataset.from_tensor_slices((low_light_images, enhanced_images))
+    dataset = dataset.map(load_data, num_parallel_calls=tf.data.AUTOTUNE)
+    dataset = dataset.batch(BATCH_SIZE, drop_remainder=True)
+    return dataset
+
+
+train_low_light_images = sorted(glob("./lol_dataset/our485/low/*"))[:MAX_TRAIN_IMAGES]
+train_enhanced_images = sorted(glob("./lol_dataset/our485/high/*"))[:MAX_TRAIN_IMAGES]
+
+val_low_light_images = sorted(glob("./lol_dataset/our485/low/*"))[MAX_TRAIN_IMAGES:]
+val_enhanced_images = sorted(glob("./lol_dataset/our485/high/*"))[MAX_TRAIN_IMAGES:]
+
+test_low_light_images = sorted(glob("./lol_dataset/eval15/low/*"))
+test_enhanced_images = sorted(glob("./lol_dataset/eval15/high/*"))
+
+
+train_dataset = get_dataset(train_low_light_images, train_enhanced_images)
+val_dataset = get_dataset(val_low_light_images, val_enhanced_images)
+
+
+print("Train Dataset:", train_dataset.element_spec)
+print("Val Dataset:", val_dataset.element_spec)
+
+
+
+
+
+
+
+def selective_kernel_feature_fusion(
+    multi_scale_feature_1, multi_scale_feature_2, multi_scale_feature_3
+):
+    channels = list(multi_scale_feature_1.shape)[-1]
+    combined_feature = layers.Add()(
+        [multi_scale_feature_1, multi_scale_feature_2, multi_scale_feature_3]
+    )
+    gap = layers.GlobalAveragePooling2D()(combined_feature)
+    channel_wise_statistics = layers.Reshape((1, 1, channels))(gap)
+    compact_feature_representation = layers.Conv2D(
+        filters=channels // 8, kernel_size=(1, 1), activation="relu"
+    )(channel_wise_statistics)
+    feature_descriptor_1 = layers.Conv2D(
+        channels, kernel_size=(1, 1), activation="softmax"
+    )(compact_feature_representation)
+    feature_descriptor_2 = layers.Conv2D(
+        channels, kernel_size=(1, 1), activation="softmax"
+    )(compact_feature_representation)
+    feature_descriptor_3 = layers.Conv2D(
+        channels, kernel_size=(1, 1), activation="softmax"
+    )(compact_feature_representation)
+    feature_1 = multi_scale_feature_1 * feature_descriptor_1
+    feature_2 = multi_scale_feature_2 * feature_descriptor_2
+    feature_3 = multi_scale_feature_3 * feature_descriptor_3
+    aggregated_feature = layers.Add()([feature_1, feature_2, feature_3])
+    return aggregated_feature
+
+
+
+
+
+class ChannelPooling(layers.Layer):
+    def __init__(self, axis=-1, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.axis = axis
+        self.concat = layers.Concatenate(axis=self.axis)
+
+    def call(self, inputs):
+        average_pooling = tf.expand_dims(tf.reduce_mean(inputs, axis=-1), axis=-1)
+        max_pooling = tf.expand_dims(tf.reduce_max(inputs, axis=-1), axis=-1)
+        return self.concat([average_pooling, max_pooling])
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"axis": self.axis})
+
+
+def spatial_attention_block(input_tensor):
+    compressed_feature_map = ChannelPooling(axis=-1)(input_tensor)
+    feature_map = layers.Conv2D(1, kernel_size=(1, 1))(compressed_feature_map)
+    feature_map = keras.activations.sigmoid(feature_map)
+    return input_tensor * feature_map
+
+
+def channel_attention_block(input_tensor):
+    channels = list(input_tensor.shape)[-1]
+    average_pooling = layers.GlobalAveragePooling2D()(input_tensor)
+    feature_descriptor = layers.Reshape((1, 1, channels))(average_pooling)
+    feature_activations = layers.Conv2D(
+        filters=channels // 8, kernel_size=(1, 1), activation="relu"
+    )(feature_descriptor)
+    feature_activations = layers.Conv2D(
+        filters=channels, kernel_size=(1, 1), activation="sigmoid"
+    )(feature_activations)
+    return input_tensor * feature_activations
+
+
+def dual_attention_unit_block(input_tensor):
+    channels = list(input_tensor.shape)[-1]
+    feature_map = layers.Conv2D(
+        channels, kernel_size=(3, 3), padding="same", activation="relu"
+    )(input_tensor)
+    feature_map = layers.Conv2D(channels, kernel_size=(3, 3), padding="same")(
+        feature_map
+    )
+    channel_attention = channel_attention_block(feature_map)
+    spatial_attention = spatial_attention_block(feature_map)
+    concatenation = layers.Concatenate(axis=-1)([channel_attention, spatial_attention])
+    concatenation = layers.Conv2D(channels, kernel_size=(1, 1))(concatenation)
+    return layers.Add()([input_tensor, concatenation])
+
+
+
+
+
+
+
+
+def down_sampling_module(input_tensor):
+    channels = list(input_tensor.shape)[-1]
+    main_branch = layers.Conv2D(channels, kernel_size=(1, 1), activation="relu")(
+        input_tensor
+    )
+    main_branch = layers.Conv2D(
+        channels, kernel_size=(3, 3), padding="same", activation="relu"
+    )(main_branch)
+    main_branch = layers.MaxPooling2D()(main_branch)
+    main_branch = layers.Conv2D(channels * 2, kernel_size=(1, 1))(main_branch)
+    skip_branch = layers.MaxPooling2D()(input_tensor)
+    skip_branch = layers.Conv2D(channels * 2, kernel_size=(1, 1))(skip_branch)
+    return layers.Add()([skip_branch, main_branch])
+
+
+def up_sampling_module(input_tensor):
+    channels = list(input_tensor.shape)[-1]
+    main_branch = layers.Conv2D(channels, kernel_size=(1, 1), activation="relu")(
+        input_tensor
+    )
+    main_branch = layers.Conv2D(
+        channels, kernel_size=(3, 3), padding="same", activation="relu"
+    )(main_branch)
+    main_branch = layers.UpSampling2D()(main_branch)
+    main_branch = layers.Conv2D(channels // 2, kernel_size=(1, 1))(main_branch)
+    skip_branch = layers.UpSampling2D()(input_tensor)
+    skip_branch = layers.Conv2D(channels // 2, kernel_size=(1, 1))(skip_branch)
+    return layers.Add()([skip_branch, main_branch])
+
+
+# MRB Block
+def multi_scale_residual_block(input_tensor, channels):
+    # features
+    level1 = input_tensor
+    level2 = down_sampling_module(input_tensor)
+    level3 = down_sampling_module(level2)
+    # DAU
+    level1_dau = dual_attention_unit_block(level1)
+    level2_dau = dual_attention_unit_block(level2)
+    level3_dau = dual_attention_unit_block(level3)
+    # SKFF
+    level1_skff = selective_kernel_feature_fusion(
+        level1_dau,
+        up_sampling_module(level2_dau),
+        up_sampling_module(up_sampling_module(level3_dau)),
+    )
+    level2_skff = selective_kernel_feature_fusion(
+        down_sampling_module(level1_dau),
+        level2_dau,
+        up_sampling_module(level3_dau),
+    )
+    level3_skff = selective_kernel_feature_fusion(
+        down_sampling_module(down_sampling_module(level1_dau)),
+        down_sampling_module(level2_dau),
+        level3_dau,
+    )
+    # DAU 2
+    level1_dau_2 = dual_attention_unit_block(level1_skff)
+    level2_dau_2 = up_sampling_module((dual_attention_unit_block(level2_skff)))
+    level3_dau_2 = up_sampling_module(
+        up_sampling_module(dual_attention_unit_block(level3_skff))
+    )
+    # SKFF 2
+    skff_ = selective_kernel_feature_fusion(level1_dau_2, level2_dau_2, level3_dau_2)
+    conv = layers.Conv2D(channels, kernel_size=(3, 3), padding="same")(skff_)
+    return layers.Add()([input_tensor, conv])
+
+
+
+
+
+
+
+
+
+
+def recursive_residual_group(input_tensor, num_mrb, channels):
+    conv1 = layers.Conv2D(channels, kernel_size=(3, 3), padding="same")(input_tensor)
+    for _ in range(num_mrb):
+        conv1 = multi_scale_residual_block(conv1, channels)
+    conv2 = layers.Conv2D(channels, kernel_size=(3, 3), padding="same")(conv1)
+    return layers.Add()([conv2, input_tensor])
+
+
+def mirnet_model(num_rrg, num_mrb, channels):
+    input_tensor = keras.Input(shape=[None, None, 3])
+    x1 = layers.Conv2D(channels, kernel_size=(3, 3), padding="same")(input_tensor)
+    for _ in range(num_rrg):
+        x1 = recursive_residual_group(x1, num_mrb, channels)
+    conv = layers.Conv2D(3, kernel_size=(3, 3), padding="same")(x1)
+    output_tensor = layers.Add()([input_tensor, conv])
+    return keras.Model(input_tensor, output_tensor)
+
+
+model = mirnet_model(num_rrg=3, num_mrb=2, channels=64)
+
+
+
+def load_model(weights_path):
+   
+    model.load_weights("LLIE.h5")
+    return model
+
+# Preprocess the image
+def preprocess_image(image):
+    # Resize image to match model input shape
+    image = image.resize((256, 256))
+    # Normalize pixel values
+    image = np.array(image) / 255.0
+    # Add batch dimension
+    image = np.expand_dims(image, axis=0)
+    return image
+
+# Perform image enhancement
+def enhance_image(model, image):
+    # Preprocess image
+    image = preprocess_image(image)
+    # Perform enhancement using the model
+    enhanced_image = model.predict(image)
+    # Postprocess the enhanced image if necessary
+    # Here you can adjust contrast, brightness, etc.
+    return enhanced_image
+
+def main():
+    st.title('Night-time Image Enhancement with MirNet')
+    
+    # Upload image
+    st.sidebar.title('Upload Image')
+    uploaded_image = st.sidebar.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+    if uploaded_image is not None:
+        # Load the pre-trained model
+        weights_path = 'LLIE'
+        model = load_model(weights_path)
+
+        
+
+        # Display original image
+        st.subheader('Original Image')
+        original_image = Image.open(uploaded_image)
+        st.image(original_image, caption='Original Image', use_column_width=True)
+
+        # Enhance image
+        st.subheader('Enhanced Image')
+        enhanced_image = enhance_image(model, original_image)
+        enhanced_image = Image.fromarray((enhanced_image[0] * 255).astype(np.uint8))
+        st.image(enhanced_image, caption='Enhanced Image', use_column_width=True)
+
+
+main()
